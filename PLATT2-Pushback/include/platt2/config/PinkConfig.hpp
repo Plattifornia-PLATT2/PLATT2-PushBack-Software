4233ba48f6b196661d26f0edbe136202b51c34c3
Dominic <67755597+09explorer@users.noreply.github.com>
2025-10-08 18:39:30 -0500

    Updated port numbers for test bot

--- PLATT2-Pushback/include/platt2/config/PinkConfig.hpp
+++ PLATT2-Pushback/include/platt2/config/PinkConfig.hpp
@@ -13,16 +13,16 @@ class PinkConfig : public IConfig {
     static constexpr pros::MotorGears DRIVE_GEARSET{pros::MotorGears::blue};
 
     static constexpr int LEFT_FRONT_MODULE_TOP_PORT{1};
-    static constexpr int LEFT_FRONT_MODULE_BOTTOM_PORT{2};
+    static constexpr int LEFT_FRONT_MODULE_BOTTOM_PORT{-2};
 
     static constexpr int RIGHT_FRONT_MODULE_TOP_PORT{3};
-    static constexpr int RIGHT_FRONT_MODULE_BOTTOM_PORT{4};
+    static constexpr int RIGHT_FRONT_MODULE_BOTTOM_PORT{-4};
 
     static constexpr int LEFT_REAR_MODULE_TOP_PORT{5};
-    static constexpr int LEFT_REAR_MODULE_BOTTOM_PORT{6};
+    static constexpr int LEFT_REAR_MODULE_BOTTOM_PORT{-6};
 
     static constexpr int RIGHT_REAR_MODULE_TOP_PORT{9};
-    static constexpr int RIGHT_REAR_MODULE_BOTTOM_PORT{8};
+    static constexpr int RIGHT_REAR_MODULE_BOTTOM_PORT{-8};
 
     public:
 
