a2621536b28634bd9c1c2d8b59a21c67cae015b3
Dominic <67755597+09explorer@users.noreply.github.com>
2026-03-18 23:31:56 -0500

    Refactor intake subsystem for a lever bot

--- PLATT2-Pushback/include/platt2/robot/subsystems/intake/IntakeSubsystem.hpp
+++ PLATT2-Pushback/include/platt2/robot/subsystems/intake/IntakeSubsystem.hpp
@@ -1,6 +1,7 @@
 #ifndef INTAKESUBSYSTEM_HPP
 #define INTAKESUBSYSTEM_HPP
 
+#include "platt2/hal/Solenoid.hpp"
 #include "pros/adi.hpp"
 #include "pros/distance.hpp"
 #include "pros/motors.hpp"
@@ -39,7 +40,7 @@ namespace intake{
         IN = 1,
         OUT = 2,
         STOP = 3,
-        OUT_LOW_GOAL=4
+        OUT_LOW_GOAL = 4
     };
 
     /**
@@ -49,41 +50,16 @@ namespace intake{
     class IntakeSubsystem{
 
         private:
-        double intake_speed{600};
-        
+        const double NORMAL_INTAKE_VOLTAGE {127};
+        const double LOW_GOAL_INTAKE_VOLTAGE {-127};
+
         // ** Motors **
         std::unique_ptr<pros::Motor> front_intake_motor;
-        std::unique_ptr<pros::Motor> rear_intake_motor;
-        std::unique_ptr<pros::Motor> middle_intake_motor;
-        std::unique_ptr<pros::Motor> upper_conveyor_motor;
-        std::unique_ptr<pros::Motor> lower_roller_motor;
-        std::unique_ptr<pros::Motor> matchload_left_motor;
-        std::unique_ptr<pros::Motor> matchload_right_motor;
- 
+        std::unique_ptr<pros::Motor> left_matchload_motor;
+        std::unique_ptr<pros::Motor> right_matchload_motor;
+
         // ** Solenoids **
-        std::unique_ptr<pros::adi::DigitalOut> ed_mech_piston;
-        std::unique_ptr<pros::adi::DigitalOut> upper_conveyor_height_piston;
-        std::unique_ptr<pros::adi::DigitalOut> conveyor_stopper_piston;
-        std::unique_ptr<pros::adi::DigitalOut> rake_mech_piston;
-        std::unique_ptr<pros::adi::DigitalOut> descore_piston;
-        std::unique_ptr<pros::adi::DigitalOut> rear_intake_piston;
-
-
-        bool conveyor_stopper_piston_state = false;
-        bool conveyor_stopper_piston_lastState = false;
-        bool ed_mech_piston_state = false;
-        bool ed_mech_piston_lastState = false;
-        bool upper_conveyor_height_piston_state = false;
-        bool upper_conveyor_height_piston_lastState = false;
-        bool rake_mech_piston_state = false;
-        bool auto_unload_active = false;
-        bool descore_piston_state = false;
-        bool colorSort = false;
-        bool matchLoading = false;
-        bool rear_intake_piston_state = false;
-        bool rear_intake_piston_lastState = false;
-
-        const double MAX_DISTANCE{100.0};
+        std::unique_ptr<hal::Solenoid> matchload_piston;
 
         public:
         /**
@@ -91,136 +67,21 @@ namespace intake{
          * 
          * @param direction The direction to move the intake motors.
          */
-        void move_intake(IntakeDirection direction);
+        void moveIntake(IntakeDirection direction);
 
         /**
-         * @brief Moves the rear intake motor in the specified direction.
-         * 
-         * @param direction The direction to move the rear intake motor.
-         */
-        void move_rear_motor(IntakeDirection direction);
-        
-        /**
-         * @brief Automatically unloads the intake subsystem.
-         */
-        void auto_unload();
-
-        /**
-         * @brief Get the rear motor position object   
-         * 
-         * @return double 
-         */
-        double get_rear_motor_position();
-
-        /**
-         * @brief Tares the rear motor position.
+         * @brief Toggles the position of the matchload pistons.
          * 
          */
-        void tare_rear_motor_position();
+        void toggleMatchloadPistons();
 
         /**
-         * @brief Checks if the auto unload feature is active.
+         * @brief Gets the current position of the matchload piston
          * 
-         * @return true When the auto unload feature is active and running.
-         * @return false When the auto unload feature is not active.
+         * @return true if extended
+         * @return false if retracted
          */
-        bool is_auto_unload_active();
-
-        /**
-         * @brief Toggles the state of the ED mechanism piston.
-         * 
-         */
-        void toggle_ed_mech_piston();
-
-        /**
-         * @brief Toggles the state of the upper conveyor height piston.
-         * 
-         */
-        void toggle_upper_conveyor_height_piston();
-
-        /**
-         * @brief Toggles the state of the conveyor stopper piston.
-         * 
-         */
-        void toggle_conveyor_stopper_piston();
-
-        /**
-         * @brief Toggles the state of the rake mechanism piston.
-         * 
-         */
-        void toggle_rake_mech_piston();
-
-        /**
-         * @brief Toggles the state of the descore piston.
-         * 
-         */
-        void toggle_descore_piston();
-
-        /**
-         * @brief Toggles the state of the rear intake (matchloader) pistons.
-         * 
-         */
-        void toggle_rear_intake_piston();
-
-        /**
-         * @brief Gets the position of the rear intake piston (matchloader).
-         * @return A boolean for the position of the rear intake state (T for out, F for in).
-         */
-        bool get_rear_intake_position();
-
-        /**
-         * @brief Sets the current position of the middle intake motor to 0.
-         */
-        void tare_middle_motor_position();
-
-        /**
-         * @brief gets the position of the middle intake motor.
-         * @return A double of the current position of the middle intake motor from its internal encoder. 
-         */
-        double get_middle_motor_position();
-
-        /**
-         * @brief Spins the intake motors in an optimized configuration for matchloading.
-         */
-        void move_intake_matchload(IntakeDirection direction);
-
-        /**
-         * @brief Sets the intake subsystem to color sort mode, or disables it.
-         * 
-         * @param state True for color sort enabled, false for color sort disabled.
-         */
-        void colorSortMode(bool state);
-
-        /**
-         * @brief Construct a new Intake Subsystem object
-         * 
-         * @param front_intake Front intake motor
-         * @param rear_intake Rear intake motor
-         * @param mid_intake Middle intake motor
-         * @param upper_conveyor Upper conveyor motor
-         * @param lower_roller Lower roller motor
-         * @param ed_mech_piston ED mechanism piston
-         * @param upper_conveyor_height_piston Upper conveyor height piston
-         * @param conveyor_stopper_piston Conveyor stopper piston
-         * @param rake_mech_piston Rake mechanism piston
-         * @param descore_piston Descore piston
-         */
-        IntakeSubsystem(
-            std::unique_ptr<pros::Motor>front_intake, 
-            std::unique_ptr<pros::Motor>rear_intake, 
-            std::unique_ptr<pros::Motor>mid_intake, 
-            std::unique_ptr<pros::Motor>upper_conveyor,
-            std::unique_ptr<pros::Motor>lower_roller,
-            std::unique_ptr<pros::Motor> matchload_left_motor,
-            std::unique_ptr<pros::Motor> matchload_right_motor,
-            std::unique_ptr<pros::adi::DigitalOut> ed_mech_piston,
-            std::unique_ptr<pros::adi::DigitalOut> upper_conveyor_height_piston,
-            std::unique_ptr<pros::adi::DigitalOut> conveyor_stopper_piston,
-            std::unique_ptr<pros::adi::DigitalOut> rake_mech_piston,
-            std::unique_ptr<pros::adi::DigitalOut> descore_piston,
-            std::unique_ptr<pros::adi::DigitalOut> rear_intake_piston
-        );
-
+        bool getMatchloadPistonState();
 
     };
 }
--- PLATT2-Pushback/src/platt2/robot/subsystems/intake/IntakeSubsystem.cpp
+++ PLATT2-Pushback/src/platt2/robot/subsystems/intake/IntakeSubsystem.cpp
@@ -11,209 +11,42 @@ namespace robot{
 namespace subsystems{
 namespace intake{
 
-    IntakeSubsystem::IntakeSubsystem(
-        std::unique_ptr<pros::Motor>front_intake, 
-        std::unique_ptr<pros::Motor>rear_intake, 
-        std::unique_ptr<pros::Motor>mid_intake, 
-        std::unique_ptr<pros::Motor>upper_conveyor,
-        std::unique_ptr<pros::Motor>lower_roller,
-        std::unique_ptr<pros::Motor> matchload_left_motor,
-        std::unique_ptr<pros::Motor> matchload_right_motor,
-        std::unique_ptr<pros::adi::DigitalOut> ed_mech_piston,
-        std::unique_ptr<pros::adi::DigitalOut> upper_conveyor_height_piston,
-        std::unique_ptr<pros::adi::DigitalOut> conveyor_stopper_piston,
-        std::unique_ptr<pros::adi::DigitalOut> rake_mech_piston,
-        std::unique_ptr<pros::adi::DigitalOut> descore_piston,
-        std::unique_ptr<pros::adi::DigitalOut> rear_intake_piston
-    )
-        {
-        front_intake_motor = std::move(front_intake);
-        rear_intake_motor = std::move(rear_intake);
-        middle_intake_motor = std::move(mid_intake);
-        upper_conveyor_motor = std::move(upper_conveyor);
-        lower_roller_motor = std::move(lower_roller);
-        this->matchload_left_motor = std::move(matchload_left_motor);
-        this->matchload_right_motor = std::move(matchload_right_motor);
-        this->ed_mech_piston = std::move(ed_mech_piston);
-        this->upper_conveyor_height_piston = std::move(upper_conveyor_height_piston);
-        this->conveyor_stopper_piston = std::move(conveyor_stopper_piston);
-        this->rake_mech_piston = std::move(rake_mech_piston);
-        this->descore_piston = std::move(descore_piston);
-        this->rear_intake_piston = std::move(rear_intake_piston);
-
-        if(rear_intake_motor){
-            rear_intake_motor->set_encoder_units(pros::E_MOTOR_ENCODER_ROTATIONS);
-        }
-
-    }
-
-    void IntakeSubsystem::move_intake(IntakeDirection direction){
+    
+    void IntakeSubsystem::moveIntake(IntakeDirection direction){
         switch(direction){
             case IN:{
-                if(colorSort){
-                    if(rear_intake_piston_state){
-                        front_intake_motor->move_velocity(-intake_speed);
-                        middle_intake_motor->move_velocity(-intake_speed);
-                        rear_intake_motor->move_velocity(intake_speed);
-                        upper_conveyor_motor->move_velocity(intake_speed);
-                        lower_roller_motor->move_velocity(intake_speed);
-                        matchload_left_motor->move_velocity(intake_speed);
-                        matchload_right_motor->move_velocity(intake_speed);
-                    }
-                    else {
-                        front_intake_motor->move_velocity(intake_speed);
-                        middle_intake_motor->move_velocity(intake_speed);
-                        rear_intake_motor->move_velocity(-intake_speed);
-                        upper_conveyor_motor->move_velocity(intake_speed);
-                        lower_roller_motor->move_velocity(-intake_speed);
-                        matchload_left_motor->move_velocity(-intake_speed);
-                        matchload_right_motor->move_velocity(-intake_speed);
-                    }
-                }
-                else{
-                    front_intake_motor->move_velocity(intake_speed);
-                    middle_intake_motor->move_velocity(intake_speed);
-                    rear_intake_motor->move_velocity(intake_speed);
-                    upper_conveyor_motor->move_velocity(intake_speed);
-                    lower_roller_motor->move_velocity(intake_speed);
-                    matchload_left_motor->move_velocity(intake_speed);
-                    matchload_right_motor->move_velocity(intake_speed);
-                }
-
+                front_intake_motor->move(NORMAL_INTAKE_VOLTAGE);
+                left_matchload_motor->move(NORMAL_INTAKE_VOLTAGE);
+                right_matchload_motor->move(NORMAL_INTAKE_VOLTAGE);
                 break;
             }
             case OUT:{
-                front_intake_motor->move_velocity(-intake_speed);
-                middle_intake_motor->move_velocity(-intake_speed);
-                rear_intake_motor->move_velocity(-intake_speed);
-                upper_conveyor_motor->move_velocity(-intake_speed);
-                lower_roller_motor->move_velocity(-intake_speed);
-                matchload_left_motor->move_velocity(-intake_speed);
-                matchload_right_motor->move_velocity(-intake_speed);
-                break;   
+                front_intake_motor->move(-NORMAL_INTAKE_VOLTAGE);
+                left_matchload_motor->move(-NORMAL_INTAKE_VOLTAGE);
+                right_matchload_motor->move(-NORMAL_INTAKE_VOLTAGE);
+                break;
             }
             case OUT_LOW_GOAL:{
-                front_intake_motor->move_velocity(-intake_speed);
-                middle_intake_motor->move_velocity(-intake_speed);
-                rear_intake_motor->move_velocity(-intake_speed/2);
-                upper_conveyor_motor->move_velocity(-intake_speed/2);   
-                lower_roller_motor->move_velocity(intake_speed);
-                matchload_left_motor->move_velocity(-intake_speed/2);
-                matchload_right_motor->move_velocity(-intake_speed/2);
+                front_intake_motor->move(LOW_GOAL_INTAKE_VOLTAGE);
+                left_matchload_motor->move(LOW_GOAL_INTAKE_VOLTAGE);
+                right_matchload_motor->move(LOW_GOAL_INTAKE_VOLTAGE);
                 break;
             }
             case STOP:{
-                front_intake_motor->move_velocity(0);
-                middle_intake_motor->move_velocity(0);
-                rear_intake_motor->move_velocity(0);
-                upper_conveyor_motor->move_velocity(0);
-                lower_roller_motor->move_velocity(0);   
-                matchload_left_motor->move_velocity(0);
-                matchload_right_motor->move_velocity(0);
+                front_intake_motor->move(0);
+                left_matchload_motor->move(0);
+                right_matchload_motor->move(0);
                 break;
             }
         }
     }
 
-    void IntakeSubsystem::toggle_ed_mech_piston(){
-        ed_mech_piston_state = !ed_mech_piston_state;
-        ed_mech_piston->set_value(ed_mech_piston_state);
+    void IntakeSubsystem::toggleMatchloadPistons(){
+        matchload_piston->toggleState();
     }
 
-    void IntakeSubsystem::toggle_upper_conveyor_height_piston(){
-        upper_conveyor_height_piston_state = !upper_conveyor_height_piston_state;
-        upper_conveyor_height_piston->set_value(upper_conveyor_height_piston_state);
-    }
-
-    void IntakeSubsystem::toggle_conveyor_stopper_piston(){
-        conveyor_stopper_piston_state = !conveyor_stopper_piston_state;
-        conveyor_stopper_piston->set_value(conveyor_stopper_piston_state);
-    }
-
-    void IntakeSubsystem::toggle_descore_piston(){
-        descore_piston_state = !descore_piston_state;
-        descore_piston->set_value(descore_piston_state);
-    }
-
-    void IntakeSubsystem::toggle_rake_mech_piston(){
-        rake_mech_piston_state = !rake_mech_piston_state;
-        rake_mech_piston->set_value(rake_mech_piston_state);
-    }
-
-    void IntakeSubsystem::move_rear_motor(IntakeDirection direction){
-        switch(direction){
-            case IN:{
-                if(rear_intake_piston_state){
-                        front_intake_motor->move_velocity(-intake_speed);
-                        middle_intake_motor->move_velocity(-intake_speed);
-                        rear_intake_motor->move_velocity(intake_speed);
-                        upper_conveyor_motor->move_velocity(intake_speed);
-                        lower_roller_motor->move_velocity(intake_speed);
-                        matchload_left_motor->move_velocity(intake_speed);
-                        matchload_right_motor->move_velocity(intake_speed);
-                    }
-                    else {
-                        front_intake_motor->move_velocity(intake_speed);
-                        middle_intake_motor->move_velocity(intake_speed);
-                        rear_intake_motor->move_velocity(-intake_speed);
-                        upper_conveyor_motor->move_velocity(intake_speed);
-                        lower_roller_motor->move_velocity(-intake_speed);
-                        matchload_left_motor->move_velocity(-intake_speed);
-                        matchload_right_motor->move_velocity(-intake_speed);
-                    }
-            }
-            case OUT:{
-                rear_intake_motor->move_velocity(-intake_speed);
-                lower_roller_motor->move_velocity(-intake_speed);
-                matchload_left_motor->move_velocity(-intake_speed);
-                matchload_right_motor->move_velocity(-intake_speed);
-                break;   
-            }
-            case STOP:{
-                rear_intake_motor->move_velocity(0);
-                lower_roller_motor->move_velocity(0);
-                matchload_left_motor->move_velocity(0);
-                matchload_right_motor->move_velocity(0);
-                break;
-            }
-        }
-    }
-
-    void IntakeSubsystem::tare_rear_motor_position(){
-        rear_intake_motor->tare_position();
-    }
-
-    double IntakeSubsystem::get_rear_motor_position(){
-        return rear_intake_motor->get_position();
-    }
-
-    void IntakeSubsystem::tare_middle_motor_position(){
-        middle_intake_motor->tare_position();
-    }
-
-    double IntakeSubsystem::get_middle_motor_position(){
-        return middle_intake_motor->get_position();
-    }  
-
-    void IntakeSubsystem::auto_unload(){
-        auto_unload_active = false;
-    }
-
-    bool IntakeSubsystem::is_auto_unload_active(){
-        return auto_unload_active;
-    }
-
-    void IntakeSubsystem::colorSortMode(bool state){
-        this->colorSort = state;
-    }
-
-    void IntakeSubsystem::toggle_rear_intake_piston(){
-        rear_intake_piston_state = !rear_intake_piston_state;
-        rear_intake_piston->set_value(rear_intake_piston_state);
-    }
-
-    bool IntakeSubsystem::get_rear_intake_position(){
-        return rear_intake_piston_state;
+    bool IntakeSubsystem::getMatchloadPistonState(){
+        return matchload_piston->getState();
     }
     
 }
