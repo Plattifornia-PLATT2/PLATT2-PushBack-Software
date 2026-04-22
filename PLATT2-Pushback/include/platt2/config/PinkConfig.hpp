a47de4d5a98904aa4f360d05204a4f045ca75d50
Dominic <67755597+09explorer@users.noreply.github.com>
2026-04-22 10:07:26 -0500

    Pink Ports

--- PLATT2-Pushback/include/platt2/config/PinkConfig.hpp
+++ PLATT2-Pushback/include/platt2/config/PinkConfig.hpp
@@ -51,29 +51,29 @@ class PinkConfig : public IConfig {
 
 
     // ** Drive Ports **
-    static constexpr int RIGHT_1_PORT{2};
-    static constexpr int RIGHT_2_PORT{-1};
-    static constexpr int RIGHT_3_PORT{-7};
-    static constexpr int RIGHT_4_PORT{8};
+    static constexpr int RIGHT_1_PORT{3};
+    static constexpr int RIGHT_2_PORT{-4};
+    static constexpr int RIGHT_3_PORT{-5};
+    static constexpr int RIGHT_4_PORT{6};
 
-    static constexpr int LEFT_1_PORT{-11};
-    static constexpr int LEFT_2_PORT{10}; 
-    static constexpr int LEFT_3_PORT{12};
-    static constexpr int LEFT_4_PORT{-9};
+    static constexpr int LEFT_1_PORT{-20};
+    static constexpr int LEFT_2_PORT{19}; 
+    static constexpr int LEFT_3_PORT{2};
+    static constexpr int LEFT_4_PORT{-1};
 
     // ** Intake Ports **
-    static constexpr int FRONT_INTAKE_MOTOR_PORT{-5};
-    static constexpr int MIDDLE_INTAKE_MOTOR_PORT{4};
-    static constexpr int REAR_INTAKE_MOTOR_PORT{-3};
-    static constexpr int LOWER_ROLLER_MOTOR_PORT{-17};
-    static constexpr int UPPER_CONVEYOR_MOTOR_PORT{20};
-    static constexpr int REAR_INTAKE_LEFT_MOTOR_PORT{19};
-    static constexpr int REAR_INTAKE_RIGHT_MOTOR_PORT{-18};
+    static constexpr int FRONT_INTAKE_MOTOR_PORT{-8};
+    static constexpr int MIDDLE_INTAKE_MOTOR_PORT{12};
+    static constexpr int REAR_INTAKE_MOTOR_PORT{9};
+    static constexpr int LOWER_ROLLER_MOTOR_PORT{-11};
+    static constexpr int UPPER_CONVEYOR_MOTOR_PORT{15};
+    static constexpr int REAR_INTAKE_LEFT_MOTOR_PORT{13};
+    static constexpr int REAR_INTAKE_RIGHT_MOTOR_PORT{-14};
     //static constexpr int DISTANCE_SENSOR_PORT{14};
     static constexpr pros::MotorGears INTAKE_GEARSET{pros::MotorGears::blue};
 
     // ** Color Sort Ports **
-    static constexpr int OPTICAL_SENSOR_PORT{13};
+    static constexpr int OPTICAL_SENSOR_PORT{10};
 
     // ** Pneumatics **
     static constexpr int ED_MECH_PISTON_PORT{1};
@@ -88,7 +88,7 @@ class PinkConfig : public IConfig {
 
     // ** Odometry Offsets **
     static constexpr int HORIZONTAL_ENCODER_PORT{20};
-    static constexpr int VERTICAL_ENCODER_PORT{-19};
+    static constexpr int VERTICAL_ENCODER_PORT{7};
     static constexpr double TRACKING_WHEEL_DIAMETER{2};
     static constexpr double HORIZONTAL_TRACKING_WHEEL_OFFSET{-.5};
     static constexpr double VERTICAL_TRACKING_WHEEL_OFFSET{-3.15};
